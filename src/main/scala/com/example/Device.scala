78cd6229641868081cb429b0127afbc8b7e00174
mstoyanovca <mstoyanovca@gmail.com>
2022-12-01 21:34:31 -0500

    added querying

--- src/main/scala/com/example/Device.scala
+++ src/main/scala/com/example/Device.scala
@@ -1,8 +1,60 @@
 package com.example
 
-import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors, LoggerOps}
+import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors, LoggerOps, TimerScheduler}
 import akka.actor.typed.{ActorRef, Behavior, PostStop, Signal}
-import com.example.DeviceManager.{DeviceRegistered, ReplyDeviceList, RequestDeviceList, RequestTrackDevice}
+
+import scala.concurrent.duration.{DurationInt, FiniteDuration}
+
+class DeviceGroup(context: ActorContext[DeviceGroup.Command], groupId: String) extends AbstractBehavior[DeviceGroup.Command](context) {
+
+  import DeviceGroup._
+  import DeviceManager.{DeviceRegistered, ReplyDeviceList, RequestAllTemperatures, RequestDeviceList, RequestTrackDevice}
+
+  private var deviceIdToActor = Map.empty[String, ActorRef[Device.Command]]
+  context.log.info("DeviceGroup {} started", groupId)
+
+  override def onMessage(msg: Command): Behavior[Command] =
+    msg match {
+      case trackMsg@RequestTrackDevice(`groupId`, deviceId, replyTo) =>
+        deviceIdToActor.get(deviceId) match {
+          case Some(deviceActor) =>
+            replyTo ! DeviceRegistered(deviceActor)
+          case None =>
+            context.log.info("Creating device actor for {}", trackMsg.deviceId)
+            val deviceActor = context.spawn(Device(groupId, deviceId), s"device-$deviceId")
+            context.watchWith(deviceActor, DeviceTerminated(deviceActor, groupId, deviceId))
+            deviceIdToActor += deviceId -> deviceActor
+            replyTo ! DeviceRegistered(deviceActor)
+        }
+        this
+      case RequestTrackDevice(gId, _, _) =>
+        context.log.warn2("Ignoring TrackDevice request for {}. This actor is responsible for {}.", gId, groupId)
+        this
+      case RequestDeviceList(requestId, gId, replyTo) =>
+        if (gId == groupId) {
+          replyTo ! ReplyDeviceList(requestId, deviceIdToActor.keySet)
+          this
+        } else
+          Behaviors.unhandled
+      case DeviceTerminated(_, _, deviceId) =>
+        context.log.info("Device actor for {} has been terminated", deviceId)
+        deviceIdToActor -= deviceId
+        this
+      case RequestAllTemperatures(requestId, gId, replyTo) =>
+        if (gId == groupId) {
+          context.spawnAnonymous(
+            DeviceGroupQuery(deviceIdToActor, requestId = requestId, requester = replyTo, 3.seconds))
+          this
+        } else
+          Behaviors.unhandled
+    }
+
+  override def onSignal: PartialFunction[Signal, Behavior[Command]] = {
+    case PostStop =>
+      context.log.info("DeviceGroup {} stopped", groupId)
+      this
+  }
+}
 
 object DeviceGroup {
   def apply(groupId: String): Behavior[Command] = Behaviors.setup(context => new DeviceGroup(context, groupId))
@@ -12,8 +64,7 @@ object DeviceGroup {
   private final case class DeviceTerminated(device: ActorRef[Device.Command], groupId: String, deviceId: String) extends Command
 }
 
-class DeviceManager(context: ActorContext[DeviceManager.Command])
-  extends AbstractBehavior[DeviceManager.Command](context) {
+class DeviceManager(context: ActorContext[DeviceManager.Command]) extends AbstractBehavior[DeviceManager.Command](context) {
 
   import DeviceManager._
 
@@ -22,7 +73,7 @@ class DeviceManager(context: ActorContext[DeviceManager.Command])
 
   override def onMessage(msg: Command): Behavior[Command] =
     msg match {
-      case trackMsg@RequestTrackDevice(groupId, _, replyTo) =>
+      case trackMsg@RequestTrackDevice(groupId, _, _) =>
         groupIdToActor.get(groupId) match {
           case Some(ref) =>
             ref ! trackMsg
@@ -69,50 +120,20 @@ object DeviceManager {
   final case class ReplyDeviceList(requestId: Long, ids: Set[String])
 
   private final case class DeviceGroupTerminated(groupId: String) extends DeviceManager.Command
-}
 
-class DeviceGroup(context: ActorContext[DeviceGroup.Command], groupId: String)
-  extends AbstractBehavior[DeviceGroup.Command](context) {
+  final case class RequestAllTemperatures(requestId: Long, groupId: String, replyTo: ActorRef[RespondAllTemperatures]) extends DeviceGroupQuery.Command with DeviceGroup.Command with DeviceManager.Command
 
-  import DeviceGroup._
+  final case class RespondAllTemperatures(requestId: Long, temperatures: Map[String, TemperatureReading])
 
-  private var deviceIdToActor = Map.empty[String, ActorRef[Device.Command]]
-  context.log.info("DeviceGroup {} started", groupId)
+  sealed trait TemperatureReading
 
-  override def onMessage(msg: Command): Behavior[Command] =
-    msg match {
-      case trackMsg@RequestTrackDevice(`groupId`, deviceId, replyTo) =>
-        deviceIdToActor.get(deviceId) match {
-          case Some(deviceActor) =>
-            replyTo ! DeviceRegistered(deviceActor)
-          case None =>
-            context.log.info("Creating device actor for {}", trackMsg.deviceId)
-            val deviceActor = context.spawn(Device(groupId, deviceId), s"device-$deviceId")
-            context.watchWith(deviceActor, DeviceTerminated(deviceActor, groupId, deviceId))
-            deviceIdToActor += deviceId -> deviceActor
-            replyTo ! DeviceRegistered(deviceActor)
-        }
-        this
-      case RequestTrackDevice(gId, _, _) =>
-        context.log.warn2("Ignoring TrackDevice request for {}. This actor is responsible for {}.", gId, groupId)
-        this
-      case RequestDeviceList(requestId, gId, replyTo) =>
-        if (gId == groupId) {
-          replyTo ! ReplyDeviceList(requestId, deviceIdToActor.keySet)
-          this
-        } else
-          Behaviors.unhandled
-      case DeviceTerminated(_, _, deviceId) =>
-        context.log.info("Device actor for {} has been terminated", deviceId)
-        deviceIdToActor -= deviceId
-        this
-    }
+  final case class Temperature(value: Double) extends TemperatureReading
 
-  override def onSignal: PartialFunction[Signal, Behavior[Command]] = {
-    case PostStop =>
-      context.log.info("DeviceGroup {} stopped", groupId)
-      this
-  }
+  case object TemperatureNotAvailable extends TemperatureReading
+
+  case object DeviceNotAvailable extends TemperatureReading
+
+  case object DeviceTimedOut extends TemperatureReading
 }
 
 object Device {
@@ -122,7 +143,7 @@ object Device {
 
   final case class ReadTemperature(requestId: Long, replyTo: ActorRef[RespondTemperature]) extends Command
 
-  final case class RespondTemperature(requestId: Long, value: Option[Double])
+  final case class RespondTemperature(requestId: Long, deviceId: String, value: Option[Double])
 
   final case class RecordTemperature(requestId: Long, value: Double, replyTo: ActorRef[TemperatureRecorded]) extends Command
 
@@ -146,7 +167,7 @@ class Device(context: ActorContext[Device.Command], groupId: String, deviceId: S
         replyTo ! TemperatureRecorded(id)
         this
       case ReadTemperature(id, replyTo) =>
-        replyTo ! RespondTemperature(id, lastTemperatureReading)
+        replyTo ! RespondTemperature(id, deviceId, lastTemperatureReading)
         this
       case Passivate =>
         Behaviors.stopped
@@ -157,5 +178,99 @@ class Device(context: ActorContext[Device.Command], groupId: String, deviceId: S
     case PostStop =>
       context.log.info2("Device actor {}-{} stopped", groupId, deviceId)
       this
+  }
+}
+
+object DeviceGroupQuery {
+  def apply(deviceIdToActor: Map[String, ActorRef[Device.Command]],
+            requestId: Long,
+            requester: ActorRef[DeviceManager.RespondAllTemperatures],
+            timeout: FiniteDuration): Behavior[Command] = {
+    Behaviors.setup { context =>
+      Behaviors.withTimers { timers =>
+        new DeviceGroupQuery(deviceIdToActor, requestId, requester, timeout, context, timers)
+      }
+    }
+  }
+
+  trait Command
+
+  private case object CollectionTimeout extends Command
+
+  final case class WrappedRespondTemperature(response: Device.RespondTemperature) extends Command
+
+  private final case class DeviceTerminated(deviceId: String) extends Command
+}
+
+class DeviceGroupQuery(deviceIdToActor: Map[String, ActorRef[Device.Command]],
+                       requestId: Long,
+                       requester: ActorRef[DeviceManager.RespondAllTemperatures],
+                       timeout: FiniteDuration,
+                       context: ActorContext[DeviceGroupQuery.Command],
+                       timers: TimerScheduler[DeviceGroupQuery.Command]) extends AbstractBehavior[DeviceGroupQuery.Command](context) {
+
+  import DeviceGroupQuery._
+  import DeviceManager.DeviceNotAvailable
+  import DeviceManager.DeviceTimedOut
+  import DeviceManager.RespondAllTemperatures
+  import DeviceManager.Temperature
+  import DeviceManager.TemperatureNotAvailable
+  import DeviceManager.TemperatureReading
+
+  timers.startSingleTimer(CollectionTimeout, CollectionTimeout, timeout)
+
+  private val respondTemperatureAdapter = context.messageAdapter(WrappedRespondTemperature.apply)
+
+  private var repliesSoFar = Map.empty[String, TemperatureReading]
+  private var stillWaiting = deviceIdToActor.keySet
+
+
+  deviceIdToActor.foreach {
+    case (deviceId, device) =>
+      context.watchWith(device, DeviceTerminated(deviceId))
+      device ! Device.ReadTemperature(0, respondTemperatureAdapter)
+  }
+
+  override def onMessage(msg: Command): Behavior[Command] =
+    msg match {
+      case WrappedRespondTemperature(response) => onRespondTemperature(response)
+      case DeviceTerminated(deviceId) => onDeviceTerminated(deviceId)
+      case CollectionTimeout => onCollectionTimout()
+    }
+
+  private def onRespondTemperature(response: Device.RespondTemperature): Behavior[Command] = {
+    val reading = response.value match {
+      case Some(value) => Temperature(value)
+      case None => TemperatureNotAvailable
+    }
+
+    val deviceId = response.deviceId
+    repliesSoFar += (deviceId -> reading)
+    stillWaiting -= deviceId
+
+    respondWhenAllCollected()
+  }
+
+  private def onDeviceTerminated(deviceId: String): Behavior[Command] = {
+    if (stillWaiting(deviceId)) {
+      repliesSoFar += (deviceId -> DeviceNotAvailable)
+      stillWaiting -= deviceId
+    }
+    respondWhenAllCollected()
+  }
+
+  private def onCollectionTimout(): Behavior[Command] = {
+    repliesSoFar ++= stillWaiting.map(deviceId => deviceId -> DeviceTimedOut)
+    stillWaiting = Set.empty
+    respondWhenAllCollected()
+  }
+
+  private def respondWhenAllCollected(): Behavior[Command] = {
+    if (stillWaiting.isEmpty) {
+      requester ! RespondAllTemperatures(requestId, repliesSoFar)
+      Behaviors.stopped
+    } else {
+      this
+    }
   }
 }
--- src/test/scala/com/example/DeviceSpec.scala
+++ src/test/scala/com/example/DeviceSpec.scala
@@ -1,7 +1,8 @@
 package com.example
 
 import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
-import com.example.DeviceManager.{DeviceRegistered, ReplyDeviceList, RequestDeviceList, RequestTrackDevice}
+import com.example.DeviceGroupQuery.WrappedRespondTemperature
+import com.example.DeviceManager._
 import org.scalatest.time.SpanSugar.convertIntToGrainOfTime
 import org.scalatest.wordspec.AnyWordSpecLike
 
@@ -43,8 +44,6 @@ class DeviceSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
       toShutDown ! Passivate
       registeredProbe.expectTerminated(toShutDown, registeredProbe.remainingOrDefault)
 
-      // using awaitAssert to retry because it might take longer for the groupActor
-      // to see the Terminated, that order is undefined
       registeredProbe.awaitAssert {
         groupActor ! RequestDeviceList(requestId = 1, groupId = "group", deviceListProbe.ref)
         deviceListProbe.expectMessage(ReplyDeviceList(requestId = 1, Set("device2")))
@@ -125,6 +124,147 @@ class DeviceSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
       val response2 = readProbe.receiveMessage()
       response2.requestId should ===(4)
       response2.value should ===(Some(55.0))
+    }
+
+    "return temperature value for working devices" in {
+      val requester = createTestProbe[RespondAllTemperatures]()
+
+      val device1 = createTestProbe[Command]()
+      val device2 = createTestProbe[Command]()
+
+      val deviceIdToActor = Map("device1" -> device1.ref, "device2" -> device2.ref)
+
+      val queryActor =
+        spawn(DeviceGroupQuery(deviceIdToActor, requestId = 1, requester = requester.ref, timeout = 3.seconds))
+
+      device1.expectMessageType[Device.ReadTemperature]
+      device2.expectMessageType[Device.ReadTemperature]
+
+      queryActor ! WrappedRespondTemperature(Device.RespondTemperature(requestId = 0, "device1", Some(1.0)))
+      queryActor ! WrappedRespondTemperature(Device.RespondTemperature(requestId = 0, "device2", Some(2.0)))
+
+      requester.expectMessage(
+        RespondAllTemperatures(
+          requestId = 1,
+          temperatures = Map("device1" -> Temperature(1.0), "device2" -> Temperature(2.0))))
+    }
+
+    "return TemperatureNotAvailable for devices with no readings" in {
+      val requester = createTestProbe[RespondAllTemperatures]()
+
+      val device1 = createTestProbe[Command]()
+      val device2 = createTestProbe[Command]()
+
+      val deviceIdToActor = Map("device1" -> device1.ref, "device2" -> device2.ref)
+
+      val queryActor =
+        spawn(DeviceGroupQuery(deviceIdToActor, requestId = 1, requester = requester.ref, timeout = 3.seconds))
+
+      device1.expectMessageType[Device.ReadTemperature]
+      device2.expectMessageType[Device.ReadTemperature]
+
+      queryActor ! WrappedRespondTemperature(Device.RespondTemperature(requestId = 0, "device1", None))
+      queryActor ! WrappedRespondTemperature(Device.RespondTemperature(requestId = 0, "device2", Some(2.0)))
+
+      requester.expectMessage(
+        RespondAllTemperatures(
+          requestId = 1,
+          temperatures = Map("device1" -> TemperatureNotAvailable, "device2" -> Temperature(2.0))))
+    }
+
+    "return DeviceNotAvailable if device stops before answering" in {
+      val requester = createTestProbe[RespondAllTemperatures]()
+
+      val device1 = createTestProbe[Command]()
+      val device2 = createTestProbe[Command]()
+
+      val deviceIdToActor = Map("device1" -> device1.ref, "device2" -> device2.ref)
+
+      val queryActor =
+        spawn(DeviceGroupQuery(deviceIdToActor, requestId = 1, requester = requester.ref, timeout = 3.seconds))
+
+      device1.expectMessageType[Device.ReadTemperature]
+      device2.expectMessageType[Device.ReadTemperature]
+
+      queryActor ! WrappedRespondTemperature(Device.RespondTemperature(requestId = 0, "device1", Some(2.0)))
+
+      device2.stop()
+
+      requester.expectMessage(
+        RespondAllTemperatures(
+          requestId = 1,
+          temperatures = Map("device1" -> Temperature(2.0), "device2" -> DeviceNotAvailable)))
+    }
+
+    "return temperature reading even if device stops after answering" in {
+      val requester = createTestProbe[RespondAllTemperatures]()
+
+      val device1 = createTestProbe[Command]()
+      val device2 = createTestProbe[Command]()
+
+      val deviceIdToActor = Map("device1" -> device1.ref, "device2" -> device2.ref)
+
+      val queryActor =
+        spawn(DeviceGroupQuery(deviceIdToActor, requestId = 1, requester = requester.ref, timeout = 3.seconds))
+
+      device1.expectMessageType[Device.ReadTemperature]
+      device2.expectMessageType[Device.ReadTemperature]
+
+      queryActor ! WrappedRespondTemperature(Device.RespondTemperature(requestId = 0, "device1", Some(1.0)))
+      queryActor ! WrappedRespondTemperature(Device.RespondTemperature(requestId = 0, "device2", Some(2.0)))
+
+      device2.stop()
+
+      requester.expectMessage(RespondAllTemperatures(requestId = 1, temperatures = Map("device1" -> Temperature(1.0), "device2" -> Temperature(2.0))))
+    }
+
+    "return DeviceTimedOut if device does not answer in time" in {
+      val requester = createTestProbe[RespondAllTemperatures]()
+
+      val device1 = createTestProbe[Command]()
+      val device2 = createTestProbe[Command]()
+
+      val deviceIdToActor = Map("device1" -> device1.ref, "device2" -> device2.ref)
+
+      val queryActor = spawn(DeviceGroupQuery(deviceIdToActor, requestId = 1, requester = requester.ref, timeout = 200.millis))
+
+      device1.expectMessageType[Device.ReadTemperature]
+      device2.expectMessageType[Device.ReadTemperature]
+
+      queryActor ! WrappedRespondTemperature(Device.RespondTemperature(requestId = 0, "device1", Some(1.0)))
+
+      // no reply from device2
+      requester.expectMessage(RespondAllTemperatures(requestId = 1, temperatures = Map("device1" -> Temperature(1.0), "device2" -> DeviceTimedOut)))
+    }
+
+    "be able to collect temperatures from all active devices" in {
+      val registeredProbe = createTestProbe[DeviceRegistered]()
+      val groupActor = spawn(DeviceGroup("group"))
+
+      groupActor ! RequestTrackDevice("group", "device1", registeredProbe.ref)
+      val deviceActor1 = registeredProbe.receiveMessage().device
+
+      groupActor ! RequestTrackDevice("group", "device2", registeredProbe.ref)
+      val deviceActor2 = registeredProbe.receiveMessage().device
+
+      groupActor ! RequestTrackDevice("group", "device3", registeredProbe.ref)
+      registeredProbe.receiveMessage()
+
+      // Check that the device actors are working
+      val recordProbe = createTestProbe[TemperatureRecorded]()
+      deviceActor1 ! RecordTemperature(requestId = 0, 1.0, recordProbe.ref)
+      recordProbe.expectMessage(TemperatureRecorded(requestId = 0))
+      deviceActor2 ! RecordTemperature(requestId = 1, 2.0, recordProbe.ref)
+      recordProbe.expectMessage(TemperatureRecorded(requestId = 1))
+      // No temperature for device3
+
+      val allTempProbe = createTestProbe[RespondAllTemperatures]()
+      groupActor ! RequestAllTemperatures(requestId = 0, groupId = "group", allTempProbe.ref)
+      allTempProbe.expectMessage(
+        RespondAllTemperatures(
+          requestId = 0,
+          temperatures =
+            Map("device1" -> Temperature(1.0), "device2" -> Temperature(2.0), "device3" -> TemperatureNotAvailable)))
     }
   }
 }
